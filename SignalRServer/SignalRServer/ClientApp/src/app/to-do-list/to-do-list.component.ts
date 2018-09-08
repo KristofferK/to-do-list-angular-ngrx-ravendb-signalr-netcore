73e37563102e1278c90e542b98e25c76988e6359
Kristoffer <fobber1995@gmail.com>
2018-09-08 19:41:16 +0200

    Add send button and receive event (SignalR)

--- SignalRServer/SignalRServer/ClientApp/src/app/to-do-list/to-do-list.component.ts
+++ SignalRServer/SignalRServer/ClientApp/src/app/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 
 @Component({
   selector: 'to-do-list',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent implements OnInit {
+  private hubConnection: HubConnection;
 
   constructor() { }
 
   ngOnInit() {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl('https://localhost:44364/toDoHub')
+      .build();
+
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+    this.hubConnection.on('TaskAdded', (task: string) => {
+      alert('Someone added a task: ' + task)
+    });
+  }
+
+  public addRandomTask() {
+    this.hubConnection.invoke('AddTask', 'My task').then(e => {
+      console.log('AddTask completed ' + e);
+    })
   }
 
 }
