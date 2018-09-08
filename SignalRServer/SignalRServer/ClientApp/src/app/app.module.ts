0f9ecbf70085351cc4cbc6692e8b5fa634021785
Kristoffer <fobber1995@gmail.com>
2018-09-08 19:17:54 +0200

    Add empty to-do-list component

--- SignalRServer/SignalRServer/ClientApp/src/app/app.module.ts
+++ SignalRServer/SignalRServer/ClientApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule
