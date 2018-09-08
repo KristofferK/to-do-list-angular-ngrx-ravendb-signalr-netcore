5ee8d4c8dd3cc7313f0403701765619a649525d7
Kristoffer <fobber1995@gmail.com>
2018-09-08 18:29:50 +0200

    Add a ToDo-hub to the SignalR server

---
+++ SignalRServer/SignalRServer/Hubs/ToDoHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hubs
+{
+    public class ToDoHub : Hub
+    {
+        public async Task AddTask(string task)
+        {
+            await Clients.All.SendAsync("TaskAdded", task);
+        }
+    }
+}
--- SignalRServer/SignalRServer/Startup.cs
+++ SignalRServer/SignalRServer/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRServer.Hubs;
 
 namespace SignalRServer
 {
@@ -33,6 +34,8 @@ namespace SignalRServer
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSignalR();  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +54,10 @@ namespace SignalRServer
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ToDoHub>("/toDoHub");
+            });
             app.UseMvc();
         }
     }
