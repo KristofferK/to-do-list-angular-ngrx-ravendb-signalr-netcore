a5e4ae9766f03e9779a84bd962563c460d7e6c28
Kristoffer <fobber1995@gmail.com>
2018-09-09 14:21:46 +0200

    Add primeng styling

--- SignalRServer/SignalRServer/ClientApp/src/app/app.module.ts
+++ SignalRServer/SignalRServer/ClientApp/src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ButtonModule,
+    InputTextModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
